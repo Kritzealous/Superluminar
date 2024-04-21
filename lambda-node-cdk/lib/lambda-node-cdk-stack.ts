9fd575cd045e67a0c497a0654a09a63c15f2d4a3
shsr <krithikasrinivasan@alumni.ie.edu>
2024-04-21 21:56:20 +0200

    first commit

--- lambda-node-cdk/lib/lambda-node-cdk-stack.ts
+++ lambda-node-cdk/lib/lambda-node-cdk-stack.ts
@@ -3,6 +3,7 @@ import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class LambdaNodeCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -12,7 +13,7 @@ export class LambdaNodeCdkStack extends Stack {
       partitionKey: { name: "id", type: AttributeType.STRING },
     });
 
-    new NodejsFunction(this, "MyFunc", {
+    const lambdaFuntion=new NodejsFunction(this, "MyFunc", {
       runtime: Runtime.NODEJS_16_X,
       handler: "handler",
       entry: "lib/lambda-node.ts",
@@ -20,5 +21,14 @@ export class LambdaNodeCdkStack extends Stack {
         TABLE_NAME: table.tableName,
       },
     });
+
+    // Add DynamoDB permissions to the Lambda function
+const policy = new PolicyStatement({
+  actions: ["dynamodb:GetItem", "dynamodb:PutItem", "dynamodb:UpdateItem", "dynamodb:DeleteItem"],
+  //actions: ["dynamodb:GetItem"],
+
+  resources: [table.tableArn],
+});
+lambdaFuntion.addToRolePolicy(policy);
   }
 }
--- lambda-node-cdk/lib/lambda-node.ts
+++ lambda-node-cdk/lib/lambda-node.ts
@@ -1,27 +1,68 @@
 import { APIGatewayProxyResult } from "aws-lambda";
-import { DynamoDB, GetItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDB, GetItemCommand, PutItemCommand } from "@aws-sdk/client-dynamodb";
 
 const tableName = process.env.TABLE_NAME;
 const dynamoDbClient = new DynamoDB({ region: "eu-central-1" });
 
-type DBItem = { item_id: string };
+type DBItem = {
+    item_id: string;
+    action: 'fetch' | 'insert';
+    data?: any;  // Additional data needed for insert
+};
 
 export const handler = async (
   event: DBItem
 ): Promise<APIGatewayProxyResult> => {
-  const command = new GetItemCommand({
-    TableName: tableName,
-    Key: {
-      id: { S: event.item_id },
-    },
-  });
-  const response = await dynamoDbClient.send(command);
+  if (event.action === 'fetch') {
+    const command = new GetItemCommand({
+      TableName: tableName,
+      Key: {
+        id: { S: event.item_id },
+      },
+    });
+    const response = await dynamoDbClient.send(command);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "ok - item found",
-      data: response.Item,
-    }),
-  };
+    if (!response.Item) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({
+          message: "Error: Item not found",
+          itemId: event.item_id,
+        }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "ok - item found",
+        data: response.Item,
+      }),
+    };
+  } else if (event.action === 'insert') {
+    const command = new PutItemCommand({
+      TableName: tableName,
+      Item: {
+        id: { S: event.item_id },
+        ...event.data
+      }
+    });
+    await dynamoDbClient.send(command);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "ok - item inserted",
+        itemId: event.item_id,
+        data: event.data,
+      }),
+    };
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Error: Action not supported",
+      }),
+    };
+  }
 };
